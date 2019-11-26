8b43211f7992518179b360d50a79095dd653e66f
rajviignesh <vickydeepak95@gmail.com>
2019-11-26 10:08:33 +0530

    Polynomial Regression V3

--- ML.py
+++ ML.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LinearRegression
 # Importing the dataset
 dataset = pd.read_csv("WeatherDATA1.csv")
 X = dataset.iloc[:, 4:7].values
-y = dataset.iloc[:, 1].values
+y = dataset.iloc[:, 2].values
 
 from sklearn.preprocessing import PolynomialFeatures
 poly_reg = PolynomialFeatures(degree=3)
@@ -15,4 +15,4 @@ pol_reg = LinearRegression()
 pol_reg.fit(X_poly, y)
 
 
-print(pol_reg.predict(poly_reg.fit_transform([[26, 12, 2019]])))
+print(pol_reg.predict(poly_reg.fit_transform([[25, 12, 2019]])))
